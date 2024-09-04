5b128a4f203af4fdb7b1e142d9d20e9d4768d434
mitchel-0968543 <0968543@hr.nl>
2024-09-04 12:30:17 +0200

    Add files via upload

---
+++ opdracht2oefening1.py
@@ -0,0 +1,9 @@
+containers_gelost = 331
+tijd_lossen = 441
+containers_geladen = 287
+tijd_laden = 295
+lostijd_per_container = tijd_lossen / containers_gelost
+laadtijd_per_container = tijd_laden / containers_geladen
+print(f"De gemiddelde lostijd bedraagt {lostijd_per_container} minuten per container")
+print(f"De gemiddelde laadtijd bedraagt {laadtijd_per_container} minuten per container")
+
