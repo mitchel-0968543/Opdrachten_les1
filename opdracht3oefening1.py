908d16b961a2bdd0aa0b80bd530c09e2c57b0579
mitchel-0968543 <0968543@hr.nl>
2024-09-23 13:19:04 +0200

    Update opdracht3oefening1.py
    Made the answers of the first question upper marks

--- opdracht3oefening1.py
+++ opdracht3oefening1.py
@@ -1,9 +1,10 @@
-connection_choice_question = input('Met welk netwerk wilt u verbonden worden: 4G, 5G of Open Wifi?')
+connection_choice_question = input('Met welk netwerk wilt u verbonden worden: 4G, 5G of Open Wifi?').upper()
+
 if connection_choice_question == "4G":
     print(f'U bent verbonden met 4G!')
 elif connection_choice_question == "5G":
     print(f'U bent verbonden met 5G!')
-elif connection_choice_question == "Open Wifi":
+elif connection_choice_question == "OPEN WIFI":
     answer_wifi = (input(f'Weet u zeker dat u met dit netwerk wil verbinden? Ja of Nee?'))
     answer = answer_wifi.upper()
     if answer == "JA":
