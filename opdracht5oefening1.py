89f417a4e4678d94fd96a174bb57652b81a41882
Mitchel <0968543@hr.nl>
2024-09-25 14:01:17 +0200

    af

--- opdracht5oefening1.py
+++ opdracht5oefening1.py
@@ -1,7 +1,16 @@
-namen = set[ 
-    "piet",
-    "JAN",
-    "Klaas",
-]
+numbers = list(range(1, 31))
 
-print(namen[2])
+for number in numbers:
+    result = ""
+    if number % 3 == 0:
+        result += "Fizz"
+    if number % 5 == 0:
+        result += "Buzz"
+    if number % 3 == 0 and number % 5 == 0:
+        result += "FizzBuzz"
+    if result == "":
+        result = number
+    print(result)
+
+
+
